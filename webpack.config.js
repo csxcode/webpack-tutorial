d4eccbace0c1f206b506c4595081c6a46915dd4a
Carlos Silva <casp.ci@gmail.com>
2017-09-18 18:09:39 -0500

    edit

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,23 @@
+var debug = true; //process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+
 module.exports = {
+  context: __dirname,
   entry: [
-    "babel-polyfill", './src/js/app.js',    
+    "babel-polyfill", './src/js/app.js',
   ],
+  devtool: debug ? "inline-sourcemap" : false,
   output: {
     path: __dirname + '/dist',
-    filename: 'bundle.js'
+    filename: 'bundle.min.js'
   },
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
       { test: /\.css$/, loader: "style-loader!css-loader" }
     ]
-  }
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
 };
